8090b4441a14aa0460674013cb0094225d1defdb
sorlien <amasoreg@gmail.com>
2019-10-27 19:32:23 +0100

    Smooth scroll to work
    Working on index.html but not other pages

--- js/index.js
+++ js/index.js
@@ -11,20 +11,33 @@ $('#overlay li').on('click', function(){
   $('#toggle').removeClass("active");
 });
 
-// Arrow scroll up
-// var viewportHeight = $(window).height(); // == 100vh
-//  (function($) {
-//       $(document).ready(function(){
-//           $(window).scroll(function(){
-//               if ($(this).scrollTop() > viewportHeight) {
-//                   $('#navbar').fadeIn(200);
-//               } else {
-//                   $('#navbar').fadeOut(200);
-//               }
-//           });
-//       });
-//   })(jQuery);
-
+//Fade in 
 AOS.init({
   once: true
 })
+
+//Smooth scroll
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
