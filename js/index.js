83d11066987df10ccfe2c2f45ffe0bd7d09ec94e
sorlien <amasoreg@gmail.com>
2019-11-05 22:03:19 +0100

    Resumé file + smooth scroll
    Resumé file added to all views.
    Scroll to work is functional in all views

--- js/index.js
+++ js/index.js
@@ -16,28 +16,21 @@ AOS.init({
   once: true
 })
 
-//Smooth scroll
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+//Scroll to link
 
-      // Store hash
-      var hash = this.hash;
+$(document).ready(function() {
+  $('html, body').hide();
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+  if (window.location.hash) {
+      setTimeout(function() {
+          $('html, body').scrollTop(0).show();
+          $('html, body').animate({
+              scrollTop: $(window.location.hash).offset().top
+              }, 800)
+      }, 0);
+  }
+  else {
+      $('html, body').show();
+  }
 });
